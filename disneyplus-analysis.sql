ba39522c0bed9e8cd8575c2fd22090b1dea63c40
ulloahilda <ulloahildaa@gmail.com>
2025-08-05 12:47:37 -0700

    disneyplus-analysis.sql

--- disneyplus-analysis.sql
+++ disneyplus-analysis.sql
@@ -19,4 +19,62 @@ ALTER TABLE disneyplus RENAME COLUMN field12 TO description;
 -- 2. Preview disneyplus table
 SELECT * FROM disneyplus LIMIT 10;
 
--- 3. 
+-- 3. Find most common genres on Disney plus
+
+
+
+
+
+-- 4. How has the number of new releases changed over the years?
+SELECT 
+	release_year,
+	COUNT (*) AS repeat_count
+FROM disneyplus 
+GROUP BY 	
+	release_year	
+Order by 
+	repeat_count DESC;
+
+--5. How has the number of new releases changed over the years TV vs. Movies?
+-- This data set shows both Movies and TV show count
+SELECT 
+	release_year,
+	type,
+	COUNT (*) AS repeat_count
+FROM disneyplus 
+GROUP BY 	
+	release_year, type	
+Order by 
+	repeat_count DESC ;
+
+-- 5. Are movies or TV shows more dominant?
+--Query to only find movies count
+SELECT 
+	release_year,
+	type,
+	COUNT (*) AS repeat_count
+FROM disneyplus 
+WHERE type = 'Movie'
+GROUP BY 	
+	release_year, type 
+Order by 
+	repeat_count DESC ;
+
+-- Query to only find TV show count 
+SELECT 
+	release_year,
+	type,
+	COUNT (*) AS repeat_count
+FROM disneyplus 
+WHERE type = 'TV Show'
+GROUP BY 	
+	release_year, type 
+Order by 
+	repeat_count DESC ;
+
+
+
+
+
+
+
